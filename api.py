0221910c016c0ca612b847cb1c28e3c3bb3bed75
forbenaj <benjaminbustelo@gmail.com>
2023-07-31 11:52:24 -0300

    Initial commit

--- api.py
+++ api.py
@@ -1,12 +1,18 @@
+#API Connecting to the Character.ai webpage
+
 from characterai import PyCAI
 
 def chat(message):
     client = PyCAI('39b1fa309f245479a9f440cebb1af394399ac90a')
-        
-    data = client.chat.send_message('tDMVnZFsQxT33IY306y5Fia_AYhfqgx3ecUYIpo6ZWQ', message)
+
+
+    #Character ID, found in the url
+    char = 'tDMVnZFsQxT33IY306y5Fia_AYhfqgx3ecUYIpo6ZWQ'
     
-    message = data['replies'][0]['text']
-    #name = data['src_char']['participant']['name']
+    #Send the message, wait for the response
+    data = client.chat.send_message(char, message)
     
-    #print(f"{name}: {message}")
-    return message
+    #Get last response
+    response = data['replies'][0]['text']
+
+    return response
