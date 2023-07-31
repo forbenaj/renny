28e4639a5f439de1ed13b88bf2b1b4a43b734201
forbenaj <benjaminbustelo@gmail.com>
2023-07-31 15:13:53 -0300

    Initial commit

--- api.py
+++ api.py
@@ -5,7 +5,6 @@ from characterai import PyCAI
 def chat(message):
     client = PyCAI('39b1fa309f245479a9f440cebb1af394399ac90a')
 
-
     #Character ID, found in the url
     char = 'tDMVnZFsQxT33IY306y5Fia_AYhfqgx3ecUYIpo6ZWQ'
     
