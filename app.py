41925901cb00e8125f32b77d3167ef74b3ac622a
forbenaj <benjaminbustelo@gmail.com>
2023-08-02 00:11:06 -0300

    Initial commit

--- app.py
+++ app.py
@@ -2,14 +2,6 @@ from api import chat
 from folder import list_files,go_back
 import json
 
-def currently(action,target):
-    if action == "stand-by":
-        print("Remmy is standing at "+path)
-    if action == "reading-file":
-        print("Remmy is checking "+target)
-    if action == "writing-journal":
-        print("Remmy is writing on his journal")
-
 def perform(action,target):
     if action == "stand-by":
         print("He wants to stand-by at "+path)
@@ -20,34 +12,29 @@ def perform(action,target):
     if action == "write-journal":
         print("He wants to write on his journal")
 
-path="G:/Benaj/Pictures"
+path="G:/Benja 2010/Imágenes Benja"
 
 files = list_files(path)
-
-firstTenFiles = files[:10]
 
 msg = f'"{path}"\n'
 
 #for file in files:
-for file in firstTenFiles:
+for file in files[:20]:
     msg = msg + file + "\n"
 
 response = chat(msg)
-
-#print(msg)
-#print(response)
 
 # Parsing the JSON string
 data = json.loads(response)
 
 # Accessing the data
-current_action = data['current-action']
-current_target = data['current-target']
+currently = data['currently']
 mind = data['mind']
 next_action = data['next-action']
-next_target = data['next-target']
+target = data['target']
+
+print(currently)
 print(mind)
 
-currently(current_action,current_target)
 
-perform(next_action,next_target)
+perform(next_action,target)
